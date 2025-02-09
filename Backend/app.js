78f10c2fe72fad4c29a3cdc1888ab9153d8a3817
Raphael Laroque <raphael.laroque@yahoo.fr>
2025-02-09 16:04:17 +0100

    maj 2025 1.0.0

--- Backend/app.js
+++ Backend/app.js
@@ -1,52 +1,57 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const helmet = require('helmet');
+const helmet = require("helmet");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const path = require('path');
+const path = require("path");
 
-const cors = require('cors');
+const cors = require("cors");
 
-const postRoutes = require('./routes/post');
+const postRoutes = require("./routes/post");
 
-const userRoutes = require('./routes/user');
-
-
+const userRoutes = require("./routes/user");
 
 // Connexion au serveur MongoDB //
-mongoose.connect('mongodb+srv://MurderSmile:DeathLaugh@cluster13.wdfny.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(
+    "mongodb+srv://MurderSmile:DeathLaugh@cluster13.wdfny.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-
-  app.use(helmet());
-  app.use(cors());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Autorisation CORS //
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization', 'access-control-allow-origin');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.setHeader('Cross-Origin-Resource-Policy', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
+    "access-control-allow-origin"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.setHeader("Cross-Origin-Resource-Policy", "*");
   next();
 });
 
-
-
 // Lien vers le fichier 'post.js' du dossier 'routes' //
-app.use('/api/posts', postRoutes);
+app.use("/api/posts", postRoutes);
 
 // Lien vers le fichier 'user.js' du dossier 'routes' //
-app.use('/api/auth', userRoutes);
+app.use("/api/auth", userRoutes);
 
 // Lien vers le répertoire 'images' //
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
